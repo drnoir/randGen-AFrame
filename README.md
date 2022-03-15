7735c23b8c0dc0ddd4183602bf1bb352aae14758
drnoir <chrispgodber@gmail.com>
2022-03-15 19:28:55 +0000

    init commit of randGenerator Component -Component core - Demos - WIP

---
+++ README.md
@@ -0,0 +1,118 @@
+## Random A-Frame Entity Generator Component ver 1.0.0
+####A-Frame componenet by Chris Godber 
+##### Released under MIT License
+#### Author - GH: @drnoir 
+
+Experimental A-Frame build to explore creating generative randomness.
+Features demo with AFrame and core component code.
+
+### Core Functionality
+- On load generate a randomised 3D sculpture with random primitives, scale, textures, colors and random positioning based on seed values
+- assign random textures if texture mode is on (Optional), random colour to each shape
+- Two main modes - Load random primitives or load random models (Optional) in glb format (in Schema textures / custumGLB - Boolean)
+- Seed values that can be modified to mod amount of randomness / spread and scale
+- Min / Max values to determine amount of minimum / Maximum shapes to generate
+- Random lights generation (Optional)
+
+### How to Use
+Include the component code in your html head with the correct path after your ref to the aFrame library
+```
+<script src="https://aframe.io/releases/1.2.0/aframe.min.js"></script>
+<script src="randGen.js"></script>
+```
+##### Make sure the component is referenced correctly otherwise it will not work (lowercase and randgen).
+
+If you do not pass it some values the component will render with the defaults.
+
+You can customise min amount of entities to render, max amount of entities to render, specify if you want to load custum
+models, wherever you want to load random lights, the randSeed value, random seed scale value, and any random texture values you 
+want to pass the component.
+
+The seed value are used throughout the component for the distribution of random positioning and scaling
+You can also load any textures and models for random distribution. The idea is that his gives a lot of 
+room for customisation and an almost infinite amount of randomness.
+
+Within your <a-scene> markup in your html file
+add a new entity with attribute randgen.(shown here with
+example values).
+
+### Example Usage
+```
+<a-entity 
+   id="randGen"
+    randgen=
+    "minShapes:40;
+    maxShapes:80;
+    custumGlb:false;
+    lights:true;
+    randSeed:4;
+    custumTexturesIDs:binary,brain,futurecity, lines,orbital,truth;
+    custumModels:custumGLB;"
+    rotation="0 0 0"
+    position="0 0 0"
+    >
+</a-entity>
+```
+#### Organising Assets for custom Textures
+if withTextures is true, you will need to pass the component  some asset ID's 
+for the textures to be applied randomly otherwise the textures will not be applied.
+
+In theory, currently you can pass any number of custom textures.
+
+In the asset section of an A-Scene
+```
+<a-assets>
+    <img id="binary" src="textures/binary.jpg">
+    <img id="brain" src="textures/brain.jpg">
+    <img id="futurecity" src="textures/futurecity.jpg">
+    <img id="lines" src="textures/lines.jpg">
+    <img id="orbital" src="textures/Orbital-State.jpg">
+    <img id="truth" src="textures/truth.jpg">
+</a-assets>
+```
+#### Passing texture IDs to component Schema Example 
+```
+custumTexturesIDs:binary,brain,futurecity, lines,orbital,truth;
+```
+#### Organising Assets for custom Models
+```
+<a-assets>
+   <a-asset-item id="custumGLB" src="./model/custum.glb"></a-asset-item>
+   <a-asset-item id="custumGLB2" src="./model/eye.glb"></a-asset-item>
+</a-assets>
+```
+#### Passing Model ID's to component Schema Example
+```
+custumModels:custumGLB, custumGLB;"
+```
+### Options and Schema Defaults
+
+#### Schema and defaults
+- ##### minShapes:10; Type : Number
+- ##### maxShapes:20; Type : Number
+- ##### custumGlb:false;  Type : Boolean
+- ##### withTextures : false; Type : Boolean
+- ##### lights:false; Type : Boolean
+- ##### randSeed:8; Type : Number
+- ##### randSeedScale:2; Type : Number
+- ##### custumTexturesIDs:[]; Type : Array
+- ##### custumModels:[]; Type : Array
+ 
+### Notes
+- You are free to put in absurdly high values for min, max and seed values, however your computer may well blow up
+at high values - over 9000 for example ;)
+- Obviously make sure any asset references to conform to your directory structure (We've all missed this tbf)
+- Currently, only glb format is accepted for custom models - I may expand it to accept obj in future as well
+
+### Further Development / Feature optionS
+- add UI for customisation of randomness / set max / min etc
+- share / export randomly generated scenes - screenshots or save the generated model information 
+- social media share buttons etc 
+
+### Possible Uses / Useful modfications 
+- reuse / reimagine methods for similar projects
+- As a helper / template project for generative art starting points in A-Frame / AR etc
+- might be useful for games like a space game where you need to generate a bunch of 
+random enemies for example
+- Generative Art Projects
+- Generate randomly distributed models for reuse (assuming exporter functionality working)
---
+++ component/randGen.js
@@ -0,0 +1,193 @@
+// Random A-Frame Entity Generator Component ver 1.0.0 by Chris Godber GH - drnoir
+// Desc: Experimental A-Frame build to explore creating generative randomness.
+
+//Functionality:
+// On load generate a randomised 3D sculpture with random primitives, scale, textures, colors and random positioning based on seed values
+// Assign random textures if texture mode is on (Optional), random colour to each shape
+// Two main modes - Load random primitives or load random models (Optional) in glb format (in Schema textures / custumGLB - Boolean)
+// Seed values that can be modified to mod amount of randomness / spread and scale
+// Min / Max values to determine amount of minimum / Maximum shapes to generate
+// Random lights generation (Optional)
+
+AFRAME.registerComponent("randgen", {
+    multiple: true,
+    schema: {
+        minShapes: {type: 'number', default: 10},
+        maxShapes: {type: 'number', default: 20},
+        custumGlb: {type: 'boolean', default: false},
+        randSeed: {type: 'number', default: 8},
+        randSeedScale: {type: 'number', default: 2},
+        withTextures: {type: 'boolean', default: true},
+        lights: {type: 'boolean', default: false},
+        custumTexturesIDs: {type: 'array', default: []},
+        custumModels: {type: 'array', default: []}
+    },
+
+    init: function () {
+        const data = this.data; const min = data.minShapes; const max = data.maxShapes;
+        const el = this.el;
+        // generate value of type number for amount of shapes based on min max vals
+        const randFun = this.genRanNum(min, max);
+        // function desc - This method creates the room
+        const artContainer = document.createElement('a-entity');
+        artContainer.setAttribute('id', 'artContainer')
+        artContainer.setAttribute('scale', '1 1 1');
+        artContainer.setAttribute('position', '0 0 0');
+        document.querySelector('a-scene').appendChild(artContainer);
+        // generate entities loop based on component schema
+        this.makeSculpture(randFun, data);
+
+    },
+
+    update: function (oldData) {
+        const data = this.data;
+        const el = this.el;
+
+        console.log("UPDATE FUNCTION HIT...");
+
+        // If `oldData` is empty, then this means we're in the initialization process.
+        // No need to update.
+        if (Object.keys(oldData).length === 0) {
+            console.log("exit");
+            return;
+        }
+
+        // Geometry-related properties changed. Update the geometry.
+        if (data.minShapes !== oldData.minShapes) {
+            this.el.setAttribute('minShapes', data.minShapes);
+            console.log("MIN CHANGE DETECTED" + data.minShapes);
+        }
+
+
+    },
+    // FLUSH
+    remove: function () {
+        // Do something the component or its entity is detached.
+    },
+    tick: function (time, timeDelta) {
+        // Do something on every scene tick or frame.
+    },
+    // generate entities
+    makeSculpture: function (amount, data) {
+        let id;
+        for (id = 0; id < amount; id++) {
+            // genearte the entities with settings
+            this.genArtwork(id, data);
+            const lights = data.lights;
+            // light generation if lights boolean is true - if ID is divisble by two (To avoid lights being generated EVERY stage of loop)
+            if (id === id % 2 && lights) {
+                let Light = document.createElement('a-entity');
+                let randLightX;let randLightY;let randLightZ;
+                let randIntensity;
+                let randRotX;let randRotY;let randRotZ;
+                // generate random pos and rotation values
+                randLightX = this.genRanNum(0, 4); randLightY = this.genRanNum(0, 1); randLightZ = this.genRanNum(0, 11);
+                randRotX = this.genRanNum(0, 45); randRotY = this.genRanNum(0, 45); randRotZ = this.genRanNum(0, 45);
+                // randomise intensity of lights
+                randIntensity = this.genRanNum(0.1, 0.6);
+                let randLightCol = this.randColor().toString;
+                // append lights to container
+                Light.setAttribute('light', 'type: spot; intensity:' + randIntensity + 'decay: 0.12; penumbra: 0.24; castShadow: true');
+                Light.setAttribute('color', '#' + randLightCol);
+                Light.setAttribute('position', {x: randLightX, y: randLightY, z: randLightZ});
+                Light.setAttribute('rotation', {x: randRotX, y: randRotY, z: randRotZ});
+                document.getElementById('artContainer').appendChild(Light);
+            }
+        }
+    },
+
+    genArtwork: function (id, data) {
+        //init rand pos vars
+        let randX;let randY;let randZ;
+        let randScaleX;let randScaleY;let randScaleZ;
+        // init randomSeed val for pos
+        let randSeed = data.randSeed;
+        let randSeedRandom = this.genRanNum(0, randSeed);
+        // init randomSeed val for Scale
+        let randSeedScale = data.randSeedScale;
+        let randSeedScaleRandom = this.genRanNum(0, randSeedScale);
+        //generate random positions and scale with genRandNum function
+        randX = this.genRanNum(0, randSeedRandom);
+        randY = this.genRanNum(0, randSeedRandom);
+        randZ = this.genRanNum(0, randSeedRandom);
+        randScaleX = this.genRanNum(0, randSeedScaleRandom);
+        randScaleY = this.genRanNum(0, randSeedScaleRandom);
+        randScaleZ = this.genRanNum(0, randSeedScaleRandom);
+
+        let custumGlb = data.custumGlb;
+        let genPiece; let randPart; let randGLB;
+        // determine if custom Obj state is true or false and generate pieces or custum GLB
+        if (!custumGlb) {
+            randPart = this.randomisePiece();
+            genPiece = document.createElement(randPart);
+        } else {
+            genPiece = document.createElement('a-entity');
+            randGLB = this.randomiseGLB(data);
+            genPiece.setAttribute('gltf-model', '#' + randGLB);
+        }
+
+        // generate the pieces and  assign randomness :~) *WOOP WOOP* - this is where it all comes together
+        let randCol = this.randColor();
+        genPiece.setAttribute('position', {x: randX, y: randY, z: randZ});
+        genPiece.setAttribute('material', 'color', '#' + randCol);
+
+        // check if textures mode is init before applying random textures function
+        let withTextures = data.withTextures;
+        if (withTextures) {
+            let randomTexture = this.randomiseTexture(data);
+            genPiece.setAttribute('material', 'src', '#' + randomTexture);
+        }
+
+        genPiece.setAttribute('scale', {x: randScaleX, y: randScaleY, z: randScaleZ});
+        genPiece.setAttribute('name', 'genPiece');
+        genPiece.setAttribute('class', 'genPiece' + id);
+        genPiece.setAttribute('roughness', this.genRanNum(0, 1));
+        genPiece.setAttribute('metalness', this.genRanNum(0, 0.2));
+        const artContainer = document.getElementById('artContainer')
+        artContainer.appendChild(genPiece);
+    },
+
+    randomisePiece: function(){
+        //possible options of primitives in Arr
+        const posPieces = ['a-box', 'a-sphere', 'a-cylinder', 'a-dodecahedron', 'a-ring', 'a-circle', 'a-triangle', 'a-torus', 'a-tetrahedron'];
+        //gen random index pos -1 is due to indexing of array starting from 0
+        let randIndx = this.genRanNum(0, posPieces.length);
+        let randPiece = posPieces[randIndx];
+        // return string from random pos generated
+        return randPiece;
+    },
+    // function to return a random texture
+    randomiseTexture: function (data) {
+        const TextureIDs = data.custumTexturesIDs;
+        //possible options of textures in Arr
+        //gen random index pos
+        let randIndx = this.genRanNum(0, TextureIDs.length);
+        let randTexture = TextureIDs[randIndx];
+        // return string from random pos generated
+        return randTexture;
+    },
+
+// function to return a random texture
+    randomiseGLB: function (data) {
+        const GLBIDs = data.custumModels;
+        //possible options of textures in Arr
+        //gen random index pos
+        let randIndx = this.genRanNum(0, GLBIDs.length);
+        let randGLB = GLBIDs[randIndx];
+        // return string from random pos generated
+        return randGLB;
+    },
+
+    randColor: function() {
+        const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+        return randomColor;
+    },
+
+    //return random values between a min / max - resuable
+    genRanNum: function (min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    },
+
+});
